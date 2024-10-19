3d78ad99baf20465eb4bd1fe31de1b821bec8b1c
Airam Santos diaz <airam@MacBook-Pro-de-Airam.local>
2024-10-19 17:55:34 +0200

    iteracion 2 completed

--- src/index.js
+++ src/index.js
@@ -6,17 +6,30 @@ let timer = null; // Variable to store the interval
 
 // ITERATION 1: Add event listener to the start button
 
-// Your code goes here ...
-
-
+const startButton = document.querySelector('#start-btn')
+startButton.addEventListener("click", () => {
+  startCountdown();
+console.log("boton clicado");
+});
 
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
+  startButton.disabled = true;
+  let remainingTime = 10;
+  const countDownInterval = setInterval (() => {
+    remainingTime--;
+    
+    if (remainingTime <= 0) {
+      clearInterval(countDownInterval);
+      showToast();
+    }
+
+    document.getElementById('time').innerText = remainingTime;
+  },1000)
   console.log("startCountdown called!");
 
 
-  // Your code goes here ...
 }
 
 
